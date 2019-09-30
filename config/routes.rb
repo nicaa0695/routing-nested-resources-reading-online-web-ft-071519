8d2e34f68b8e150b54e200eeb3e7968587d09fc6
Veronica Lopez <nicaa0695@gmail.com>
2019-09-30 15:20:13 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :authors, only: [:show]
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+  
+  get 'authors/:id/posts'
+  get 'authors/:id/posts/:post_id'
 
   root 'posts#index'
 
